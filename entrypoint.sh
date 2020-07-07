b36c8e9fa7b9158c81b7b102a744931c2d3175cd
Maxence 'Linutux' POULAIN <linutux42@users.noreply.github.com>
2020-07-07 13:30:08 +0200

    Update path of dhcpd binary

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/dhcpd -4 -f -d --no-pid -cf /etc/dhcp/dhcpd.conf -lf /var/lib/dhcp/dhcpd.leases
+/usr/sbin/dhcpd -4 -f -d --no-pid -cf /etc/dhcp/dhcpd.conf -lf /var/lib/dhcp/dhcpd.leases
