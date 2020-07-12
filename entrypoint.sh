f3518c2180332688ef3f145832f7aa381fb63e63
Maxence 'Linutux' POULAIN <linutux42@users.noreply.github.com>
2020-07-12 12:14:01 +0200

    Update entrypoint with variables and checks for the lease file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-/usr/sbin/dhcpd -4 -f -d --no-pid -cf /etc/dhcp/dhcpd.conf -lf /var/lib/dhcp/dhcpd.leases
+data_dir="/etc/dhcp"
+
+uid=$(stat -c%u "$data_dir")
+gid=$(stat -c%g "$data_dir")
+if [ $gid -ne 0 ]; then
+  groupmod -g $gid dhcpd
+fi
+if [ $uid -ne 0 ]; then
+  usermod -u $uid dhcpd
+fi
+
+[ -e "$data_dir/dhcpd.leases" ] || touch "$data_dir/dhcpd.leases"
+  chown dhcpd:dhcpd "$data_dir/dhcpd.leases"
+if [ -e "$data_dir/dhcpd.leases~" ]; then
+  chown dhcpd:dhcpd "$data_dir/dhcpd.leases~"
+fi
+
+/usr/sbin/dhcpd -4 -f -d --no-pid -cf "$data_dir/dhcpd.conf" -lf "$data_dir/dhcpd.leases"
